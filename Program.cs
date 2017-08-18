f25d22427399a94a2885e6fcc35efd9de8eaf7d8
Pinsorn <pongsan.chatsang@mail.kmutt.ac.th>
2017-08-18 22:55:22 +0700

    First Publish
    I'm maybe done.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statcal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Statcal.StatisticCalculate Calc = new StatisticCalculate { };
+            String Message="";
+            Boolean Loop = true;
+            String Memory_Display = "";
+            int Memory_Index = 0;
+            while (Loop == true) {
+                Console.Clear();
+                //var Calc_Member_Tostring = Calc.Memory;
+                foreach (Double Calc_Member_ToString in Calc.Memory) {
+                    Memory_Display = Memory_Display + "[" + Memory_Index + "]" + Calc_Member_ToString + " ";
+                    Memory_Index++;
+                }
+                Console.WriteLine("Enter some number to add it to the List or use following command : FindMode FindMed FindMean FindSD FindMax FindMin Edit Exit"+ System.Environment.NewLine+"List Of Number"+Memory_Display + System.Environment.NewLine + Message+ System.Environment.NewLine);
+                String Function_Command = Console.ReadLine();
+                if (Function_Command.ToLower() == "findmed") {
+                    Message = "Median = " + Calc.FindMedian();
+                }
+                else if (Function_Command.ToLower() == "findmode")
+                {
+                    Message ="Mode = " + Calc.FindMode();
+                }
+                else if (Function_Command.ToLower() == "findsd")
+                {
+                    Message = "SD = " + Calc.FindSD();
+                }
+                else if (Function_Command.ToLower() == "findmean")
+                {
+                    Message ="Mean = " + Calc.FindMean();
+                }
+                else if (Function_Command.ToLower() == "findmax")
+                {
+                    Message ="Max = " + Calc.FindMax();
+                }
+                else if (Function_Command.ToLower() == "findmin")
+                {
+                    Message ="Min = " + Calc.FindMin();
+                }
+                else if (Function_Command.ToLower() == "exit")
+                {
+                    Loop = false;
+                }
+                else if (Function_Command.ToLower() == "edit")
+                {
+                    Console.WriteLine("usage Edit Index , Del Index or Clear else will cancel");
+                    String Input = Console.ReadLine();
+                    if (Input.ToLower().StartsWith("edit ")==true) {
+                        int IndextoEdit = Convert.ToInt32(Input.Replace("edit ", ""));
+                        Console.WriteLine("Enter number to replace at the index");
+                        double NumbertoReplace = Convert.ToDouble(Console.ReadLine());
+                        Calc.Edit(IndextoEdit, NumbertoReplace);
+                        Message = "Index " + IndextoEdit + " Edited to " + NumbertoReplace;
+                    }
+                    if (Input.ToLower().StartsWith("del ") == true)
+                    {
+                        int IndextoEdit = Convert.ToInt32(Input.Replace("del ", ""));
+                        //double NumbertoReplace = Convert.ToDouble(Console.ReadLine());
+                        Calc.Delete(IndextoEdit);
+                        Message = "Index " + IndextoEdit + " Has been deleted";
+                    }
+                    if (Input == "clear")
+                    {
+                        // int IndextoEdit = Convert.ToInt32(Input.Replace("edit ", ""));
+                        //double NumbertoReplace = Convert.ToDouble(Console.ReadLine());
+                        Calc.Clear();
+                        Message = "Memory has been cleared";
+                    }
+                    //Message = "Don't Leave the input blank";
+                }
+                else if (Function_Command == "")
+                {
+                    Message = "Don't Leave the input blank";
+                }
+                else { try {
+                        Calc.Enter(Convert.ToDouble(Function_Command));
+                        Message =Function_Command + " Has added to List";
+
+                    } catch (Exception ex) { Message = ex.ToString(); /*throw ex;/* Loop = false; Console.ReadLine(); */} }
+                Memory_Index = 0;
+                Memory_Display = "";
+            }
+            
+        }
+    }
+}
---
+++ StatisticCalculate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Statcal
+{
+    class StatisticCalculate
+    {
+        public List<double> Memory { get; } = new List<double> { };
+        public double FindMode()
+        {
+            return (Memory.GroupBy(n => n).OrderByDescending(g => g.Count()).Select(g => g.Key).FirstOrDefault());
+        }
+        public void Enter(double Number_To_Add)
+        {
+            Memory.Add(Number_To_Add);
+
+        }
+        public double FindMean() {
+            return(Memory.Average());
+        }
+        public double FindMedian()
+        {
+            List<double> MemorySort = Memory;
+            MemorySort.Sort();
+            //Memory.Count();
+            double MedIndex = (Memory.Count()+1)/2;
+            double ZEROPOINTFIVE = 0.5;
+            double result = -1;
+            if (MedIndex % 1 != 0) {// Int32 MedIndexLower = MedIndex - ZEROPOINTFIVE;
+              result =  (MemorySort[Convert.ToInt32(MedIndex - ZEROPOINTFIVE)]+ MemorySort[Convert.ToInt32(MedIndex + ZEROPOINTFIVE)])/2;
+                
+            }
+            if (MedIndex % 1 == 0)
+            {
+                result = MemorySort[Convert.ToInt32(MedIndex)];
+
+            }
+                return (result);
+        }
+        public double FindMax()
+        {
+            return (Memory.Max());
+        }
+        public double FindMin()
+        {
+            return (Memory.Min());
+        }
+        public void Edit(int IndexToEdit, double NumberToReplace) {
+            Memory[IndexToEdit] = NumberToReplace;
+
+        }
+        public void Delete(int IndexToDelete) {
+            Memory.RemoveAt(IndexToDelete);
+        }
+        public void Clear() {
+            Memory.Clear();
+        }
+        public double FindSD()
+        {
+            double average = Memory.Average();
+            double sumOfSquaresOfDifferences = Memory.Select(val => (val - average) * (val - average)).Sum();
+            double sd = Math.Sqrt(sumOfSquaresOfDifferences / Memory.Count);
+            return (sd);
+        }
+
+
+    }
+}
